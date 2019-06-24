e21092a3950ceb2e7f691f9a424d45e6c78e98f9
Amelie DUVERNET <amelieonline@gmail.com>
2019-06-24 22:59:52 +0200

    Adding TmBase

---
+++ src/DataFixtures/TmBaseStackingData.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amelaye
+ * Date: 2019-06-24
+ * Time: 22:29
+ */
+
+namespace App\DataFixtures;
+
+
+class TmBaseStackingData
+{
+
+}
---
+++ src/Entity/TmBaseStacking.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amelaye
+ * Date: 2019-06-24
+ * Time: 22:24
+ */
+
+namespace App\Entity;
+
+
+class TmBaseStacking
+{
+
+}
